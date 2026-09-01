eb5aab5cc20587802082bf7fe96cd596fe26d79f
Merie-mBf <meriembouffi2005@gmail.com>
2026-09-01 16:29:41 +0100

    remove node_modules and add gitignore

---
+++ Operateurs/Challenge9.js
@@ -0,0 +1,14 @@
+var prompt = require('prompt-sync')();
+
+let x1 = Number(prompt('x1 :  '));
+let y1 = Number(prompt('y1 :  '));
+let z1 = Number(prompt('z1 :  '));
+console.log("Position A :("+ x1 +","+ y1 +","+ z1 +")" );
+
+let x2 = Number(prompt('x2 :  '));
+let y2 = Number(prompt('y2 :  '));
+let z2 = Number(prompt('z2 :  '));
+console.log("Position B :("+ x2 +","+ y2 +","+ z2 +")" );
+
+let Distance =
+
